cc7eb125a5a1c28f4131e4befa9f11d85c5877aa
Yoonwoojae <dbsdnwp1004@naver.com>
2019-06-01 22:08:11 +0900

    1

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # wetube
 youtube clone
+
+## Pages:
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- controller/videoController.js
+++ controller/videoController.js
@@ -1,7 +1,15 @@
 export const home = (req, res) => res.render("Home", { pageTitle: "Home" });
-export const search = (req, res) => res.render("Search", { pageTitle: "Search" });
+
+export const search = (req, res) => {
+    const {query: { term: searchingBy }} = req;
+    res.render("Search", { pageTitle: "Search", searchingBy: searchingBy });
+}
+
 export const videos = (req, res) => res.render("Videos", { pageTitle: "Videos" });
 export const upload = (req, res) => res.render("Upload", { pageTitle: "Upload" });
+
 export const videoDetail = (req, res) => res.render("VideoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (req, res) => res.render("EditVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (req, res) => res.render("DeleteVideo", { pageTitle: "Delete Video" });
